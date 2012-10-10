3af0c0f87c990b6cde0d55bc1237d57dc65634f3
mochizuki shinya <mochizuki@lepidum.co.jp>
2012-10-10 16:00:57 +0900

    add project and settings for optimal jar
    > android:test-device
    [info] testing jp.co.lepidum.mochizuki.apptestbyscala.tests (3 tests)
    [info] passed:jp.co.lepidum.mochizuki.apptestbyscala.tests.ActivityTests#testHelloWorldIsShown
    [info] passed:jp.co.lepidum.mochizuki.apptestbyscala.tests.AndroidTests#testPackageIsCorrect
    [success] All tests passed
    [success] Total time: 2 s, completed 2012/10/10 16:59:29
    
    generateScalaLibraryJar project as a workbench for generating optimal scala-library.jar.
    
    First, copy classes from both main project and test project. copyClassDirectories task key do it.
    (Note: For simplicity, this process is not completely.
           Because libraries in both projects (may using scala classes) is not considered.
           You should add more settings If need.)
    
    Second, shrink classes normally by ProGuard with options like 7ddde7c.
    
    Third, retrieve scala classes from shrinked jar and zip those.
    
    Finally, build apk with optimal jar instead of default scala-library.jar.
    Be careful, classes that is needed in tests is shrinked if shrink normally by ProGuard.
    For avoid it, add proguard options in main project.
    
    That's all!

--- project/Build.scala
+++ project/Build.scala
@@ -28,11 +28,62 @@ object General {
     )
 }
 
+object GenerateShrinkedScalaLibraryJar {
+  def copyClassDirectoriesTask(main: Project, test: Project): Project.Initialize[Task[Unit]] = (classDirectory in Compile, classDirectory in Compile in main, classDirectory in Compile in test) map { (dst, src0, src1) =>
+    IO.delete(dst)
+    IO.copyDirectory(src0, dst)
+    IO.copyDirectory(src1, dst)
+  }
+
+  def filterJar(minjar: File): File = {
+    def retrievePackage(rootDir: File, file: File): String = {
+      file.getAbsolutePath.stripPrefix(rootDir.getAbsolutePath).stripPrefix(java.io.File.separator)
+    }
+
+    val tmpDir = new File(minjar.getParent, "tmp")
+    val filter = new SimpleFilter(_.startsWith("scala")) | new ExactFilter("library.properties")
+    val files = IO.unzip(minjar, tmpDir, filter)
+    val output = new File(minjar.getParent, "scala-library.shrinked.jar")
+    val sources = files.map { file => file -> retrievePackage(tmpDir, file) }
+    IO.zip(sources, output)
+    IO.delete(tmpDir)
+    output
+  }
+
+  val copyClassDirectories = TaskKey[Unit]("copy-class-directories")
+  val generateShrinkedScalaLibraryJar = TaskKey[Option[File]]("generate-shrinked-scala-library-jar")
+
+  def settings(main: Project, test: Project) = Seq (
+    proguardOption in Android += """
+    | -keep class scala.ScalaObject
+    | -keepclasseswithmembers class scala.reflect.ScalaSignature { <methods>; }
+    """.stripMargin,
+    copyClassDirectories <<= copyClassDirectoriesTask(main, test),
+    generateShrinkedScalaLibraryJar <<= (proguard in Android) map { minjar =>
+      minjar.map(filterJar(_))
+    } dependsOn(copyClassDirectories)
+  )
+
+  def mainProjectSettings(gen: Project) = Seq (
+    proguardOption in Android += "-keep class scala.** { <fields>; <methods>; }",
+    proguardInJars in Android <<= (proguardInJars in Android, scalaInstance, generateShrinkedScalaLibraryJar in gen) map { (jars, scalaInstance, shrinked) =>
+      jars.filterNot(_ == scalaInstance.libraryJar) :+ shrinked.getOrElse(scalaInstance.libraryJar)
+    }
+  )
+
+  lazy val testProjectSettings = Seq (
+    proguardInJars in Android <<= (proguardInJars in Android, scalaInstance) map { (jars, scalaInstance) =>
+      jars.filterNot(_ == scalaInstance.libraryJar)
+    }
+  )
+}
+
 object AndroidBuild extends Build {
   lazy val main = Project (
     "ApplicationTestByScala",
     file("."),
-    settings = General.fullAndroidSettings
+    settings = General.fullAndroidSettings ++
+               GenerateShrinkedScalaLibraryJar.mainProjectSettings(generateScalaLibraryJar)
   )
 
   lazy val tests = Project (
@@ -40,15 +91,18 @@ object AndroidBuild extends Build {
     file("tests"),
     settings = General.settings ++
                AndroidTest.androidSettings ++
-               General.proguardSettings ++ Seq (
-      name := "ApplicationTestByScalaTests",
-      proguardInJars in Android <<= (proguardInJars in Android, scalaInstance) map { (jars, scalaInstance) =>
-        jars.filterNot(_ == scalaInstance.libraryJar)
-      },
-      proguardOption in Android in main += """
-      | -keep class scala.ScalaObject
-      | -keep class scala.reflect.ScalaSignature { <methods>; }
-      """.stripMargin
+               General.proguardSettings ++
+               GenerateShrinkedScalaLibraryJar.testProjectSettings ++ Seq (
+      name := "ApplicationTestByScalaTests"
     )
   ) dependsOn main
+
+  lazy val generateScalaLibraryJar: Project = Project (
+    "generate-scala-library-jar",
+    file("generate-scala-library-jar"),
+    settings = General.settings ++
+               AndroidProject.androidSettings ++
+               General.proguardSettings ++
+               GenerateShrinkedScalaLibraryJar.settings(main, tests)
+  )
 }
